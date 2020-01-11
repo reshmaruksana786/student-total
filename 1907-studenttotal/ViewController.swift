f48709ddd10f38930524d266c232fdc743c80a33
reshma <reshmaruksana786786@gmail.com>
2020-01-11 11:50:50 +0530

    total for students

--- 1907-studenttotal/ViewController.swift
+++ 1907-studenttotal/ViewController.swift
@@ -12,6 +12,728 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+            let reshmaTelMarks:UInt8 = 65
+            let reshmaHinMarks:UInt8 = 64
+            let reshmaEngMarks:UInt8 = 69
+            let reshmaMathsMarks:UInt8 = 70
+            let reshmaSciMarks:UInt8 = 68
+            let reshmaSocialMarks:UInt8 = 63
+     
+        
+        
+        let reshmaTenthTotal:UInt16 = UInt16(reshmaTelMarks)+UInt16(reshmaHinMarks)+UInt16(reshmaEngMarks)+UInt16(reshmaMathsMarks)+UInt16(reshmaSciMarks)+UInt16(reshmaSocialMarks)
+        
+            print("reshma scored \(reshmaTenthTotal) in Tenth")
+        
+        
+        if(reshmaTelMarks >= 40)
+        {
+            print("reshma passed in telugu")
+        }
+        else
+        {
+            print("reshma failed in telugu")
+        }
+        
+        
+        if(reshmaHinMarks >= 40)
+        {
+            print("reshma passed in hindi")
+        }
+        else
+        {
+            print("reshma failed in hindi")
+        }
+        
+        if(reshmaEngMarks >= 40)
+        {
+            print("reshma passed in eng")
+        }
+        else
+        {
+            print("reshma failed in eng")
+        }
+        
+        if(reshmaMathsMarks >= 40)
+        {
+            print("reshma passed in maths")
+        }
+        else
+        {
+            print("reshma failed in maths")
+        }
+        
+        if(reshmaSciMarks >= 40)
+        {
+            print("reshma passed in sci")
+        }
+        else
+        {
+            print("reshma failed in sci")
+        }
+        
+        if(reshmaSocialMarks >= 40)
+        {
+            print("reshma passed in social")
+        }
+        else
+        {
+            print("reshma failed in social")
+        }
+        
+        
+        let ruksanaTelMarks:UInt8 = 66
+        let ruksanaHinMarks:UInt8 = 65
+        let ruksanaEngMarks:UInt8 = 64
+        let ruksanaMathsMarks:UInt8 = 63
+        let ruksanaSciMarks:UInt8 = 62
+        let ruksanaSocialMarks:UInt8 = 61
+        
+        
+        let ruksanaTenthTotal:UInt16 = UInt16(ruksanaTelMarks)+UInt16(ruksanaHinMarks)+UInt16(ruksanaEngMarks)+UInt16(ruksanaMathsMarks)+UInt16(ruksanaSciMarks)+UInt16(ruksanaSocialMarks)
+        
+        print("ruksana scored \(ruksanaTenthTotal) in Tenth")
+        
+        
+        if(ruksanaTelMarks >= 40)
+        {
+            print("ruksana passed in telugu")
+        }
+        else
+        {
+            print("ruksana failed in telugu")
+        }
+        
+        
+        if(ruksanaHinMarks >= 40)
+        {
+            print("ruksana passed in hindi")
+        }
+        else
+        {
+            print("ruksana failed in hindi")
+        }
+        
+        if(ruksanaEngMarks >= 40)
+        {
+            print("ruksana passed in eng")
+        }
+        else
+        {
+            print("ruksana failed in eng")
+        }
+        
+        if(ruksanaMathsMarks >= 40)
+        {
+            print("ruksana passed in maths")
+        }
+        else
+        {
+            print("ruksana failed in maths")
+        }
+        
+        if(ruksanaSciMarks >= 40)
+        {
+            print("ruksana passed in sci")
+        }
+        else
+        {
+            print("ruksana failed in sci")
+        }
+        
+        if(ruksanaSocialMarks >= 40)
+        {
+            print("ruksana passed in social")
+        }
+        else
+        {
+            print("ruksana failed in social")
+        }
+        
+        
+        
+        
+        let riyazTelMarks:UInt8 = 71
+        let riyazHinMarks:UInt8 = 72
+        let riyazEngMarks:UInt8 = 73
+        let riyazMathsMarks:UInt8 = 74
+        let riyazSciMarks:UInt8 = 75
+        let riyazSocialMarks:UInt8 = 76
+        
+        
+        let riyazTenthTotal:UInt16 = UInt16(riyazTelMarks)+UInt16(riyazHinMarks)+UInt16(riyazEngMarks)+UInt16(riyazMathsMarks)+UInt16(riyazSciMarks)+UInt16(riyazSocialMarks)
+        
+        print("riyaz scored \(riyazTenthTotal) in Tenth")
+        
+        
+        if(riyazTelMarks >= 40)
+        {
+            print("riyaz passed in telugu")
+        }
+        else
+        {
+            print("riyaz failed in telugu")
+        }
+        
+        
+        if(riyazHinMarks >= 40)
+        {
+            print("riyaz passed in hindi")
+        }
+        else
+        {
+            print("riyaz failed in hindi")
+        }
+        
+        if(riyazEngMarks >= 40)
+        {
+            print("riyaz passed in eng")
+        }
+        else
+        {
+            print("riyaz failed in eng")
+        }
+        
+        if(riyazMathsMarks >= 40)
+        {
+            print("riyaz passed in maths")
+        }
+        else
+        {
+            print("riyaz failed in maths")
+        }
+        
+        if(riyazSciMarks >= 40)
+        {
+            print("riyaz passed in sci")
+        }
+        else
+        {
+            print("riyaz failed in sci")
+        }
+        
+        if(riyazSocialMarks >= 40)
+        {
+            print("riyaz passed in social")
+        }
+        else
+        {
+            print("riyaz failed in social")
+        }
+        
+        
+        
+        
+        let rohitTelMarks:UInt8 = 81
+        let rohitHinMarks:UInt8 = 82
+        let rohitEngMarks:UInt8 = 83
+        let rohitMathsMarks:UInt8 = 84
+        let rohitSciMarks:UInt8 = 85
+        let rohitSocialMarks:UInt8 = 86
+        
+        
+        let rohitTenthTotal:UInt16 = UInt16(rohitTelMarks)+UInt16(rohitHinMarks)+UInt16(rohitEngMarks)+UInt16(rohitMathsMarks)+UInt16(rohitSciMarks)+UInt16(rohitSocialMarks)
+        
+        print("rohit scored \(rohitTenthTotal) in Tenth")
+        
+        
+        if(rohitTelMarks >= 40)
+        {
+            print("rohit passed in telugu")
+        }
+        else
+        {
+            print("rohit failed in telugu")
+        }
+        
+        
+        if(rohitHinMarks >= 40)
+        {
+            print("rohit passed in hindi")
+        }
+        else
+        {
+            print("rohit failed in hindi")
+        }
+        
+        if(rohitEngMarks >= 40)
+        {
+            print("rohit passed in eng")
+        }
+        else
+        {
+            print("rohit failed in eng")
+        }
+        
+        if(rohitMathsMarks >= 40)
+        {
+            print("rohit passed in maths")
+        }
+        else
+        {
+            print("rohit failed in maths")
+        }
+        
+        if(rohitSciMarks >= 40)
+        {
+            print("rohit passed in sci")
+        }
+        else
+        {
+            print("rohit failed in sci")
+        }
+        
+        if(rohitSocialMarks >= 40)
+        {
+            print("rohit passed in social")
+        }
+        else
+        {
+            print("rohit failed in social")
+        }
+        
+        
+        
+        
+        let rahulTelMarks:UInt8 = 91
+        let rahulHinMarks:UInt8 = 92
+        let rahulEngMarks:UInt8 = 93
+        let rahulMathsMarks:UInt8 = 94
+        let rahulSciMarks:UInt8 = 95
+        let rahulSocialMarks:UInt8 = 96
+        
+        
+        let rahulTenthTotal:UInt16 = UInt16(rahulTelMarks)+UInt16(rahulHinMarks)+UInt16(rahulEngMarks)+UInt16(rahulMathsMarks)+UInt16(rahulSciMarks)+UInt16(rahulSocialMarks)
+        
+        print("rahul scored \(rahulTenthTotal) in Tenth")
+        
+        
+        if(rahulTelMarks >= 40)
+        {
+            print("rahul passed in telugu")
+        }
+        else
+        {
+            print("rahul failed in telugu")
+        }
+        
+        
+        if(rahulHinMarks >= 40)
+        {
+            print("rahul passed in hindi")
+        }
+        else
+        {
+            print("rahul failed in hindi")
+        }
+        
+        if(rahulEngMarks >= 40)
+        {
+            print("rahul passed in eng")
+        }
+        else
+        {
+            print("rahul failed in eng")
+        }
+        
+        if(rahulMathsMarks >= 40)
+        {
+            print("rahul passed in maths")
+        }
+        else
+        {
+            print("rahul failed in maths")
+        }
+        
+        if(rahulSciMarks >= 40)
+        {
+            print("rahul passed in sci")
+        }
+        else
+        {
+            print("rahul failed in sci")
+        }
+        
+        if(rahulSocialMarks >= 40)
+        {
+            print("rahul passed in social")
+        }
+        else
+        {
+            print("rahul failed in social")
+        }
+        
+        
+        
+        
+        let ramTelMarks:UInt8 = 63
+        let ramHinMarks:UInt8 = 25
+        let ramEngMarks:UInt8 = 35
+        let ramMathsMarks:UInt8 = 46
+        let ramSciMarks:UInt8 = 55
+        let ramSocialMarks:UInt8 = 66
+        
+        
+        let ramTenthTotal:UInt16 = UInt16(ramTelMarks)+UInt16(ramHinMarks)+UInt16(ramEngMarks)+UInt16(ramMathsMarks)+UInt16(ramSciMarks)+UInt16(ramSocialMarks)
+        
+        print("ram scored \(ramTenthTotal) in Tenth")
+        
+        
+        if(ramTelMarks >= 40)
+        {
+            print("ram passed in telugu")
+        }
+        else
+        {
+            print("ram failed in telugu")
+        }
+        
+        
+        if(ramHinMarks >= 40)
+        {
+            print("ram passed in hindi")
+        }
+        else
+        {
+            print("ram failed in hindi")
+        }
+        
+        if(ramEngMarks >= 40)
+        {
+            print("ram passed in eng")
+        }
+        else
+        {
+            print("ram failed in eng")
+        }
+        
+        if(ramMathsMarks >= 40)
+        {
+            print("ram passed in maths")
+        }
+        else
+        {
+            print("ram failed in maths")
+        }
+        
+        if(ramSciMarks >= 40)
+        {
+            print("ram passed in sci")
+        }
+        else
+        {
+            print("ram failed in sci")
+        }
+        
+        if(ramSocialMarks >= 40)
+        {
+            print("ram passed in social")
+        }
+        else
+        {
+            print("ram failed in social")
+        }
+        
+        
+        
+        
+        let ranaTelMarks:UInt8 = 81
+        let ranaHinMarks:UInt8 = 85
+        let ranaEngMarks:UInt8 = 75
+        let ranaMathsMarks:UInt8 = 66
+        let ranaSciMarks:UInt8 = 45
+        let ranaSocialMarks:UInt8 = 38
+        
+        
+        let ranaTenthTotal:UInt16 = UInt16(ranaTelMarks)+UInt16(ranaHinMarks)+UInt16(ranaEngMarks)+UInt16(ranaMathsMarks)+UInt16(ranaSciMarks)+UInt16(ranaSocialMarks)
+        
+        print("rana scored \(ranaTenthTotal) in Tenth")
+        
+        
+        if(ranaTelMarks >= 40)
+        {
+            print("rana passed in telugu")
+        }
+        else
+        {
+            print("rana failed in telugu")
+        }
+        
+        
+        if(ranaHinMarks >= 40)
+        {
+            print("rana passed in hindi")
+        }
+        else
+        {
+            print("rana failed in hindi")
+        }
+        
+        if(ranaEngMarks >= 40)
+        {
+            print("rana passed in eng")
+        }
+        else
+        {
+            print("rana failed in eng")
+        }
+        
+        if(ranaMathsMarks >= 40)
+        {
+            print("rana passed in maths")
+        }
+        else
+        {
+            print("rana failed in maths")
+        }
+        
+        if(ranaSciMarks >= 40)
+        {
+            print("rana passed in sci")
+        }
+        else
+        {
+            print("rana failed in sci")
+        }
+        
+        if(ranaSocialMarks >= 40)
+        {
+            print("rana passed in social")
+        }
+        else
+        {
+            print("rana failed in social")
+        }
+        
+        
+        
+        
+        let rakeshTelMarks:UInt8 = 91
+        let rakeshHinMarks:UInt8 = 75
+        let rakeshEngMarks:UInt8 = 28
+        let rakeshMathsMarks:UInt8 = 94
+        let rakeshSciMarks:UInt8 = 95
+        let rakeshSocialMarks:UInt8 = 35
+        
+        
+        let rakeshTenthTotal:UInt16 = UInt16(rakeshTelMarks)+UInt16(rakeshHinMarks)+UInt16(rakeshEngMarks)+UInt16(rakeshMathsMarks)+UInt16(rakeshSciMarks)+UInt16(rakeshSocialMarks)
+        
+        print("rakesh scored \(rahulTenthTotal) in Tenth")
+        
+        
+        if(rakeshTelMarks >= 40)
+        {
+            print("rakesh passed in telugu")
+        }
+        else
+        {
+            print("rakesh failed in telugu")
+        }
+        
+        
+        if(rakeshHinMarks >= 40)
+        {
+            print("rakesh passed in sci")
+        }
+        else
+        {
+            print("rakesh failed in sci")
+        }
+        
+        if(rakeshEngMarks >= 40)
+        {
+            print("rakesh passed in eng")
+        }
+        else
+        {
+            print("rakesh failed in eng")
+        }
+        
+        if(rakeshMathsMarks >= 40)
+        {
+            print("rakesh passed in maths")
+        }
+        else
+        {
+            print("rakesh failed in maths")
+        }
+        
+        if(rakeshSciMarks >= 40)
+        {
+            print("rakesh passed in sci")
+        }
+        else
+        {
+            print("rakesh failed in sci")
+        }
+        
+        if(rakeshSocialMarks >= 40)
+        {
+            print("rakesh passed in social")
+        }
+        else
+        {
+            print("rakesh failed in social")
+        }
+        
+        
+        
+        
+        let roshanTelMarks:UInt8 = 63
+        let roshanHinMarks:UInt8 = 25
+        let roshanEngMarks:UInt8 = 35
+        let roshanMathsMarks:UInt8 = 46
+        let roshanSciMarks:UInt8 = 55
+        let roshanSocialMarks:UInt8 = 66
+        
+        
+        let roshanTenthTotal:UInt16 = UInt16(roshanTelMarks)+UInt16(roshanHinMarks)+UInt16(roshanEngMarks)+UInt16(roshanMathsMarks)+UInt16(roshanSciMarks)+UInt16(roshanSocialMarks)
+        
+        print("roshan scored \(roshanTenthTotal) in Tenth")
+        
+        
+        if(roshanTelMarks >= 40)
+        {
+            print("roshan passed in telugu")
+        }
+        else
+        {
+            print("roshan failed in telugu")
+        }
+        
+        
+        if(roshanHinMarks >= 40)
+        {
+            print("roshan passed in  hindi")
+        }
+        else
+        {
+            print("roshan failed in hindi")
+        }
+        
+        if(roshanEngMarks >= 40)
+        {
+            print("roshan passed in eng")
+        }
+        else
+        {
+            print("roshan failed in eng")
+        }
+        
+        if(roshanMathsMarks >= 40)
+        {
+            print("roshan passed in maths")
+        }
+        else
+        {
+            print("roshan failed in maths")
+        }
+        
+        if(roshanSciMarks >= 40)
+        {
+            print("roshan passed in sci")
+        }
+        else
+        {
+            print("roshan failed in sci")
+        }
+        
+        if(roshanSocialMarks >= 40)
+        {
+            print("roshan passed in social")
+        }
+        else
+        {
+            print("roshan failed in social")
+        }
+        
+        
+        
+        
+        let ramuTelMarks:UInt8 = 81
+        let ramuHinMarks:UInt8 = 84
+        let ramuEngMarks:UInt8 = 67
+        let ramuMathsMarks:UInt8 = 40
+        let ramuSciMarks:UInt8 = 41
+        let ramuSocialMarks:UInt8 = 35
+        
+        
+        let ramuTenthTotal:UInt16 = UInt16(ramuTelMarks)+UInt16(ramuHinMarks)+UInt16(ramuEngMarks)+UInt16(ramuMathsMarks)+UInt16(ramuSciMarks)+UInt16(ramuSocialMarks)
+        
+        print("ramu scored \(ramuTenthTotal) in Tenth")
+        
+        
+        if(ramuTelMarks >= 40)
+        {
+            print("ramu passed in telugu")
+        }
+        else
+        {
+            print("ramu failed in telugu")
+        }
+        
+        
+        if(ramuHinMarks >= 40)
+        {
+            print("ramu passed in hindi")
+        }
+        else
+        {
+            print("ramu failed in hindi")
+        }
+        
+        if(ramuEngMarks >= 40)
+        {
+            print("ramu passed in eng")
+        }
+        else
+        {
+            print("ramu failed in eng")
+        }
+        
+        if(ramuMathsMarks >= 40)
+        {
+            print("ramu passed in maths")
+        }
+        else
+        {
+            print("ramu failed in maths")
+        }
+        
+        if(ramuSciMarks >= 40)
+        {
+            print("ramu passed in sci")
+        }
+        else
+        {
+            print("ramu failed in sci")
+        }
+        
+        if(ramuSocialMarks >= 40)
+        {
+            print("ramu passed in social")
+        }
+        else
+        {
+            print("ramu failed in social")
+
+        }
+        
+        
+        
+        let englishmarks:UInt8 = 45
+        let telugumarks :UInt8 = 55
+        let hindimarks:UInt8 = 45
+        let mathsmarks :UInt8 = 55
+        let scimarks:UInt8 = 45
+        
+        let totalMarks:UInt16 = UInt16(telugumarks+englishmarks+hindimarks+mathsmarks+scimarks)
+            print(totalMarks)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
